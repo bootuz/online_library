0527800a741ddcc7c566e7ce3701f3e4a69b5bc4
Nart Kenobi <bootuz07@gmail.com>
2019-01-09 19:55:53 +0300

    Nothing special

--- books/models.py
+++ books/models.py
@@ -8,7 +8,7 @@ from .helper import rename_and_path
 
 class OverwriteStorage(FileSystemStorage):
 
-    def get_available_name(self, name, max_length):
+    def get_available_name(self, name, max_length=None):
         if self.exists(name):
             os.remove(os.path.join(settings.MEDIA_ROOT, name))
         return name
