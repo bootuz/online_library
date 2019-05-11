02bca9204599d535c88886ec4461f82381a59224
Nart Kenobi <bootuz07@gmail.com>
2019-05-11 17:05:09 +0300

    setting.py added secret key env

--- library/settings.py
+++ library/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e9c#k_)^dk!hqxmd5lf!5ao05z5!0iy-nv7=%a-i17_*0p_yop'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
