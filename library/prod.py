d61aba8637318efbcd3338c231a0ce907f2c2928
Nart Kenobi <bootuz07@gmail.com>
2019-09-27 14:22:41 +0300

    updated prod.py

--- library/prod.py
+++ library/prod.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['nart.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'www.baraeja.com']
 
 
 # Application definition
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'library.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ['DB_NAME'],
+        'NAME': "nart$default",
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASSWORD'],
         'HOST': os.environ['DB_HOST'],
